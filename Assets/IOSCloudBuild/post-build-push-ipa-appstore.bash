260e332dbede178993a3bd7b49d264461d656fd7
MaroLFC <m.hesham@cryptydgames.com>
2023-05-08 19:35:56 +0300

    Changed Username to m.hesham@cryptydgames.com

--- Assets/IOSCloudBuild/post-build-push-ipa-appstore.bash
+++ Assets/IOSCloudBuild/post-build-push-ipa-appstore.bash
@@ -5,7 +5,7 @@ echo "Uploading IPA to Appstore Connect..."
 path="${UNITY_PLAYER_PATH}"
 if xcrun altool --upload-app --type ios -f $path -u a.alaa@cryptydgames.com -p eqcj-eavm-jbtj-gcfr ; then
     echo "Upload IPA to Appstore Connect finished with success"
-    watchbuild -a com.cryptyd.balootQuest2 -u a.alaa@cryptydgames.com --slack_url https://hooks.slack.com/services/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
+    watchbuild -a com.cryptyd.balootQuest2 -u m.hesham@cryptydgames.com --slack_url https://hooks.slack.com/services/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
 else
     echo "Upload IPA to Appstore Connect failed"
 fi
